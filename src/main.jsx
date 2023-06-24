06a4e1b49121d7cf66dbc0de719e83303218af81
Fran <franencabo@gmail.com>
2023-06-24 22:04:48 +0200

    Router and diferent pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import './style.css'
+import './styles.css'
 import { HeroesApp } from './HeroesApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
